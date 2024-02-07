8c162ca20f87d4b688d1ca32732bbafd91b30771
pl251052 <pl251052@ahschool.com>
2024-02-07 08:29:24 -0500

    invisibilty cloak

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
+import time
+video_capture = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc('m','p','4','v')# note the lower case
+frame_width = int(video_capture.get(3))
+frame_height = int(video_capture.get(4))
+out = cv2.VideoWriter('Harry_Potter.mp4',fourcc , 10, (frame_width,frame_height), True)
+time.sleep(3)
+background = 0
+while (video_capture.isOpened()):
+    ret, image = video_capture.read()
+    if not ret:
+        break
+    image = np.flip(image, axis=1)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    # Change to HSV
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+#Create masks with coordinates to detect the color
+    low = np.array([0, 42, 0])
+    high = np.array([179, 255, 255])
+    mask = cv2.inRange(hsv_frame, low, high)
+    mask_all = cv2.bitwise_and(frame, frame, mask=mask)
+    mask_all = cv2.morphologyEx(mask_all, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
+    mask_all = cv2.morphologyEx(mask_all, cv2.MORPH_DILATE, np.ones((3,3),np.uint8))
+# Hide the blue part away
+    mask2 = cv2.bitwise_not(mask_all)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    streamA = cv2.bitwise_and(image, image, mask=mask2)
+# Copy the masked area's original part
+    streamB = cv2.bitwise_and(background, background, mask=mask_all)
+#Write the video in the file specified in the previous block
+    output = cv2.addWeighted(streamA,1,streamB,1,0)
+    out.write(output)
+    cv2.imshow("cloak_trick",output)
+    if cv2.waitKey(25) == 13:
+        break
+video_capture.release()
+out.release()
+cv2.destroyAllWindows()
